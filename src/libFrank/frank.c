f74ff386930114c9974b229f6b16ceba3d95163d
StevenSYS <139715581+StevenSYS@users.noreply.github.com>
2025-03-20 11:19:45 +0000

    Now you can build libFrank on its own

--- src/libFrank/frank.c
+++ src/libFrank/frank.c
@@ -19,6 +19,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "lists.h"
+
 #define sizeofArray(_array) sizeof(_array) / sizeof(*_array)
 #define includesString(_str1, _str2) strstr(_str1, _str2) != NULL
 #define arrayRandom(_array) _array[rand() % sizeofArray(_array)]
@@ -30,130 +32,6 @@ char frank_response[MAX_FRANK_STRLENGTH] = "Frank is waiting...";
 char isLocked = 0;
 unsigned char angerLevel = 0;
 const unsigned char angerThreshold = 5;
-
-const char *typoVariations[] = {
-	"can't",
-	"cant",
-	"won't",
-	"wont",
-	"don't",
-	"dont"
-};
-
-const char *positiveRatResponses[] = {
-	"Frank is interested.",
-	"Frank edges closer, tongue flicking.",
-	"Frank's pupils dilate with excitement.",
-	"Frank starts licking profusely.",
-	"Frank perks up instantly.",
-	"Frank watches intently, coiled and ready.",
-	"Frank slithers forward, very intrigued.",
-};
-
-const char *highAngerResponses[] = {
-	"Frank hisses fiercely, her eyes narrowing with disdain.",
-	"Frank coils tightly, flicking her tongue in irritation.",
-	"Frank's scales ripple as she fixes you with a cold glare.",
-	"Frank flicks her tongue sharply, showing clear displeasure."
-};
-
-const char *feedingFailureResponses[] = {
-	"Frank misses completely.",
-	"Frank lunges forward and bonks the glass.",
-	"Frank strikes but bites herself."
-};
-
-const char *neutralResponses[] = {
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank says nothing.",
-	"Frank stares at you, unamused.",
-	"Frank is too tired to care.",
-	"Frank sighs.",
-	"Frank curls up and ignores you.",
-	"Frank stares at the ceiling.",
-	"Frank gives you a blank look.",
-	"Frank pretends she didn’t hear that.",
-	"Frank looks the other way.",
-	"Frank is not impressed.",
-	"Frank is too busy doing nothing.",
-	"Frank gives you a slow, unblinking stare.",
-	"Frank is clearly not interested.",
-	"Frank is busy... doing absolutely nothing.",
-	"Frank yawns.",
-	"Frank doesn’t even bother to look at you.",
-	"Frank slithers off without saying a word.",
-	"Frank just sits there. Silent.",
-	"Frank rolls her eyes (if snakes could roll their eyes).",
-	"Frank stares at you like you've said something foolish.",
-	"Frank gives you a long, silent stare.",
-	"Frank sneezes in your general direction.",
-	"Frank doesn’t even flinch.",
-	"Frank is having none of this.",
-	"Frank seems to have fallen asleep.",
-	"Frank hisses softly and says nothing.",
-	"Frank looks bored.",
-	"Frank flicks her tongue, then ignores you.",
-	"Frank just sits there. Existing.",
-	"Frank doesn’t dignify that with a response.",
-	"Frank slithers into the void.",
-	"Frank seems to have mentally checked out.",
-	"Frank stares into the middle distance.",
-	"Frank stares blankly at the wall.",
-	"Frank gives you a look of mild disdain.",
-	"Frank is doing her best to look uninterested."
-};
-
-const char *sadResponses[] = {
-	"Frank coils up, heartbroken.",
-	"Frank hisses softly and flicks her tongue.",
-	"Frank stares at you with a cold, silent glare.",
-	"Frank flicks her tongue in clear disappointment.",
-	"Frank disappears into a tight coil, sulking."
-};
-
-const char *boopingSnootResponses[] = {
-	"Frank recoils instantly, her eyes narrowing.",
-	"Frank hisses and coils back.",
-	"Frank flinches, her gaze filled with betrayal.",
-	"Frank flicks her tongue sharply in disapproval.",
-	"Frank stares at you coldly. Don’t do that again.",
-	"Frank freezes, her posture radiating displeasure.",
-	"Frank actually dislikes snoot boops."
-};
-
-const char *boopingSnootWords[] = {
-	"boop",
-	"snoot boop",
-	"booping snoot",
-	"boop your snoot"
-};
 
 static char isBoopingSnoot(char *input) {
 	char found = 0;
